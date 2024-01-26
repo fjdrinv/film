ffa78eb64759111986fc297e44169870cca363b5
Aziz <dev@tararoutray.com>
2024-01-26 14:12:47 +0500

    first commit

---
+++ script/script.js
@@ -0,0 +1,22 @@
+AOS.init({
+    duration: 2000,
+});
+
+const navBtn = document.querySelector(".nav_btn");
+const navList = document.querySelector(".films");
+const bg = document.querySelector(".nav_bg");
+const body = document.querySelector("body");
+
+navBtn.addEventListener("click", () => {
+  navList.classList.toggle("active");
+  navBtn.classList.toggle("active");
+  bg.classList.toggle("active");
+  body.classList.toggle("active");
+});
+
+bg.addEventListener("click", () => {
+  bg.classList.remove("active");
+  navList.classList.remove("active");
+  navBtn.classList.remove("active");
+  body.classList.remove("active");
+});
